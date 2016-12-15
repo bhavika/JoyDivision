8237fa70e93b60f676c56a5a692574bd42e3a215
Bhavika Tekwani <bhavicka.7992@gmail.com>
2016-12-15 14:31:12 -0500

    more experiments

--- src/create_dataset.py
+++ src/create_dataset.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import numpy as np
 from sklearn.preprocessing import StandardScaler
+from sklearn import preprocessing
 
 
 numerical = ['Tempo', 'Loudness', 'Energy', 'Speechiness', 'Danceability', 'Acousticness', 'Instrumentalness']
@@ -15,6 +16,7 @@ master = pd.merge(master, features, on='TrackID', how='inner')
 master = pd.merge(master, labels, on='File', how='inner')
 master = master.rename(index=str, columns={"Artist_x": "Artist", "Title_x": "Title"})
 
+
 trackids_2 = pd.read_csv('../data/trackids_new_2.csv', sep=';')
 features_2 = pd.read_csv('../data/features_2.csv', sep=';')
 subset_2 = pd.read_pickle('../data/newsubset.pkl')
@@ -26,20 +28,9 @@ d = d.rename(index=str, columns={"Artist_x": "Artist", "Title_x": "Title"})
 master = pd.concat([master, d])
 master = master.drop_duplicates(subset=['File', 'Artist', 'Title'], keep='first')
 
-# print master['Tempo'].mean(), master['Tempo'].std()
-# print master['Loudness'].mean(), master['Loudness'].std()
-# print master['Energy'].mean(), master['Energy'].std()
-# print master['Danceability'].mean(), master['Danceability'].std()
-# print master['Acousticness'].mean(), master['Acousticness'].std()
-# print master['Valence'].mean(), master['Valence'].std()
-# print master['Instrumentalness'].mean(), master['Instrumentalness'].std()
-# print master['Speechiness'].mean(), master['Speechiness'].std()
-
-
 master['KeyMode'] = master['Key'] * master['Mode']
 master['LoudnessSq'] = master['Loudness'] * master['Loudness']
 master['TempoMode'] = master['Tempo'] * master['Mode']
-
 
 
 # Impute missing values
@@ -50,18 +41,20 @@ master['Valence'].fillna(master['Valence'].mean(), inplace=True)
 master['Instrumentalness'].fillna(master['Instrumentalness'].mean(), inplace=True)
 master['Speechiness'].fillna(master['Speechiness'].mean(), inplace=True)
 
+# Create segment features
 
 
-def get_beatcount(x):
+def get_shape(x):
     return x.shape[0]
 
 
+# timbre features of shape (1, 90)
 def compute_timbre_feature(x):
     features = x.T
     flen = features.shape[1]
     ndim = features.shape[0]
     assert ndim == 12, "Transpose error - wrong dimension"
-    finaldim = 90
+    #finaldim = 90
     if flen < 3:
         print "flen < 3"
         return None
@@ -77,28 +70,17 @@ def compute_timbre_feature(x):
     return f
 
 
+# pitch average feature vector of size (1, 12)
 def compute_pitch_feature(x):
     features = x.T
     flen = features.shape[1]
     ndim = features.shape[0]
-
     assert ndim == 12, "Transpose error - wrong dimension"
-    finaldim = 90
     if flen < 3:
         print "flen < 3"
         return None
     avg = np.average(features, 1)
-    # cov = np.cov(features)
-    covflat = []
-    # for k in range(12):
-    #     covflat.extend(np.diag(cov, k))
-    # covflat = np.array(covflat, dtype=object)
-    # concatenate avg and cov
-    # f = np.concatenate([avg, covflat])
-
     f = list([avg])
-    # print avg
-    # print len(avg)
     return avg
 
 
@@ -114,53 +96,59 @@ def compute_timbre_average(x):
     avg = np.average(features, 1)
     return avg
 
+
+def compute_timbre_avg_var(x):
+    features = x.T
+    flen = features.shape[1]
+    ndim = features.shape[0]
+    assert ndim == 12, "Transpose error - wrong dimension"
+    variance = np.var(features, 1)
+    if flen < 3:
+        print "flen < 3"
+        return None
+    avg = np.average(features, 1)
+    varflat = []
+    for k in range(12):
+        varflat.extend(np.diag(variance, k))
+    f = np.concatenate([avg, varflat])
+    f = list(f)
+    return f
+
+
 master['TimbreVector'] = master['Timbre'].apply(lambda x: compute_timbre_feature(x))
 master['PitchVector'] = master['Pitches'].apply(lambda x: compute_pitch_feature(x))
 master['TimbreAverage'] = master['Timbre'].apply(lambda x: compute_timbre_average(x))
+master['TimbreAvgVariance'] = master['Timbre'].apply(lambda x: compute_timbre_avg_var(x))
+
 
 timbrevec = master['TimbreVector'].apply(pd.Series)
 timbrevec = timbrevec.rename(columns = lambda x: 'tim_'+str(x))
 master = pd.concat([master[:], timbrevec[:]], axis=1)
 
-
 timbreavg = master['TimbreAverage'].apply(pd.Series)
 timbreavg = timbreavg.rename(columns= lambda x: 'timavg_'+str(x))
 master = pd.concat([master[:], timbreavg[:]], axis=1)
-
 
 pitchvec = master['PitchVector'].apply(pd.Series)
 pitchvec = pitchvec.rename(columns = lambda x: 'pitch_'+str(x))
 master = pd.concat([master[:], pitchvec[:]], axis=1)
 
-filter_col = [col for col in list(master.columns.values) if col.startswith('pitch_')]
+timbrevariances = master['TimbreAvgVariance'].apply(pd.Series)
+timbrevariances = timbrevariances.rename(columns = lambda x: 'timvar_'+str(x))
+master = pd.concat([master[:], timbrevariances[:]], axis=1)
 
-master['Beats'] = master['BeatsConfidence'].apply(lambda x: get_beatcount(x))
-# Scaling values
+master['Beats'] = master['BeatsConfidence'].apply(lambda x: get_shape(x))
+master['MinutesLong'] = master['Length'] / 60
+master['NoOfSegments'] = master['Pitches'].apply(lambda x: get_shape(x))
 
-master[numerical].apply(lambda x: StandardScaler().fit_transform(x))
+master['AvgLoudnessTimbre'] = master['timavg_1']
+master['AvgBrightnessTimbre'] = master['timavg_2']
+master['AvgFlatnessTimbre'] = master['timavg_3']
+master['AvgAttackTimbre'] = master['timavg_4']
+
+
+print master.columns.values
 
 
 master.to_pickle('../data/fullset.pkl')
 
-
-# ['File' 'Artist' 'Title' 'TimeSignature' 'Key' 'SegmentsLoudMax' 'Mode'
-#  'BeatsConfidence' 'Length' 'Tempo' 'Loudness' 'Timbre' 'Pitches'
-#  'KeyConfidence' 'Artist_y' 'Title_y' 'TrackID' 'Energy' 'Speechiness'
-#  'Valence' 'Danceability' 'Acousticness' 'Instrumentalness' 'Mood'
-#  'KeyMode' 'LoudnessSq' 'TempoMode' 'TimbreVector' 'PitchVector'
-#  'TimbreAverage' 'tim_0' 'tim_1' 'tim_2' 'tim_3' 'tim_4' 'tim_5' 'tim_6'
-#  'tim_7' 'tim_8' 'tim_9' 'tim_10' 'tim_11' 'tim_12' 'tim_13' 'tim_14'
-#  'tim_15' 'tim_16' 'tim_17' 'tim_18' 'tim_19' 'tim_20' 'tim_21' 'tim_22'
-#  'tim_23' 'tim_24' 'tim_25' 'tim_26' 'tim_27' 'tim_28' 'tim_29' 'tim_30'
-#  'tim_31' 'tim_32' 'tim_33' 'tim_34' 'tim_35' 'tim_36' 'tim_37' 'tim_38'
-#  'tim_39' 'tim_40' 'tim_41' 'tim_42' 'tim_43' 'tim_44' 'tim_45' 'tim_46'
-#  'tim_47' 'tim_48' 'tim_49' 'tim_50' 'tim_51' 'tim_52' 'tim_53' 'tim_54'
-#  'tim_55' 'tim_56' 'tim_57' 'tim_58' 'tim_59' 'tim_60' 'tim_61' 'tim_62'
-#  'tim_63' 'tim_64' 'tim_65' 'tim_66' 'tim_67' 'tim_68' 'tim_69' 'tim_70'
-#  'tim_71' 'tim_72' 'tim_73' 'tim_74' 'tim_75' 'tim_76' 'tim_77' 'tim_78'
-#  'tim_79' 'tim_80' 'tim_81' 'tim_82' 'tim_83' 'tim_84' 'tim_85' 'tim_86'
-#  'tim_87' 'tim_88' 'tim_89' 'timavg_0' 'timavg_1' 'timavg_2' 'timavg_3'
-#  'timavg_4' 'timavg_5' 'timavg_6' 'timavg_7' 'timavg_8' 'timavg_9'
-#  'timavg_10' 'timavg_11' 'pitch_0' 'pitch_1' 'pitch_2' 'pitch_3' 'pitch_4'
-#  'pitch_5' 'pitch_6' 'pitch_7' 'pitch_8' 'pitch_9' 'pitch_10' 'pitch_11'
-#  'Beats']
---
+++ src/feature_combinations.py
@@ -0,0 +1,182 @@
+import pandas as pd
+import numpy as np
+from explore_features import train
+from sklearn.ensemble import ExtraTreesClassifier
+import matplotlib.pyplot as plt
+from time import time
+
+timbre_avg = [col for col in list(train.columns.values) if col.startswith('timavg_')]
+timbre = [col for col in list(train.columns.values) if col.startswith('tim_')]
+pitch_col = [col for col in list(train.columns.values) if col.startswith('pitch_')]
+top_4_timbre = ['AvgLoudnessTimbre', 'AvgBrightnessTimbre', 'AvgFlatnessTimbre', 'AvgAttackTimbre']
+desc_features = ['Danceability', 'Energy', 'Tempo', 'LoudnessSq', 'Acousticness', 'Instrumentalness', 'Speechiness']
+notational_features = ['Mode', 'KeyMode', 'TimeSignature', 'TempoMode', 'Beats']
+
+comb1 = top_4_timbre + pitch_col + desc_features + notational_features
+comb2 = top_4_timbre + desc_features
+comb3 = desc_features + notational_features
+comb4 = pitch_col + top_4_timbre + desc_features
+comb5 = desc_features
+
+
+start = time()
+
+
+print "Combination 1: top 4 timbres, pitch averages, desc and notational features"
+
+X = train[comb1]
+y = train['Mood']
+
+
+forest = ExtraTreesClassifier(n_estimators=250)
+forest.fit(X,y)
+
+importances = forest.feature_importances_
+std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
+indices = np.argsort(importances)[::-1]
+
+features_neworder = [comb1[i] for i in indices]
+
+print features_neworder
+print ("Feature ranking:")
+
+for f in range(X.shape[1]):
+    print ("%d. feature %d %s (%f)" % (f + 1, indices[f], comb1[indices[f]],  importances[indices[f]]))
+
+plt.figure()
+plt.title("Feature importance")
+plt.bar(range(X.shape[1]), importances[indices], color="b", yerr=std[indices], align="center")
+plt.xticks(range(X.shape[1]), features_neworder)
+plt.xlim([-1, X.shape[1]])
+plt.savefig('../explore/comb1_features.png')
+
+
+print "Done with combination 1"
+
+
+print "Combination 2- Top 4 timbres and Desc features -----------------------------------"
+
+X = train[comb2]
+y = train['Mood']
+
+
+forest = ExtraTreesClassifier(n_estimators=250)
+forest.fit(X,y)
+
+importances = forest.feature_importances_
+std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
+indices = np.argsort(importances)[::-1]
+
+features_neworder = [comb2[i] for i in indices]
+
+print features_neworder
+print ("Feature ranking for combination 2: ")
+
+for f in range(X.shape[1]):
+    print ("%d. feature %d %s (%f)" % (f + 1, indices[f], comb2[indices[f]],  importances[indices[f]]))
+
+plt.figure()
+plt.title("Feature importance for combination 2")
+plt.bar(range(X.shape[1]), importances[indices], color="b", yerr=std[indices], align="center")
+plt.xticks(range(X.shape[1]), features_neworder)
+plt.xlim([-1, X.shape[1]])
+plt.savefig('../explore/comb2_features.png')
+
+print "Done with combination 2"
+
+
+print "Combination 3 - Notational features & Desc features -----------------------------------"
+
+X = train[comb3]
+y = train['Mood']
+
+
+forest = ExtraTreesClassifier(n_estimators=250)
+forest.fit(X,y)
+
+importances = forest.feature_importances_
+std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
+indices = np.argsort(importances)[::-1]
+
+features_neworder = [comb3[i] for i in indices]
+
+print features_neworder
+print ("Feature ranking for combination 3: ")
+
+for f in range(X.shape[1]):
+    print ("%d. feature %d %s (%f)" % (f + 1, indices[f], comb3[indices[f]],  importances[indices[f]]))
+
+plt.figure()
+plt.title("Feature importance for combination 3")
+plt.bar(range(X.shape[1]), importances[indices], color="b", yerr=std[indices], align="center")
+plt.xticks(range(X.shape[1]), features_neworder)
+plt.xlim([-1, X.shape[1]])
+plt.savefig('../explore/comb3_features.png')
+
+print "Done with combination 3"
+
+print "Combination 4 - Pitch, Timbre and Desc -----------------------------------"
+
+X = train[comb4]
+y = train['Mood']
+
+
+forest = ExtraTreesClassifier(n_estimators=250)
+forest.fit(X,y)
+
+importances = forest.feature_importances_
+std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
+indices = np.argsort(importances)[::-1]
+
+features_neworder = [comb4[i] for i in indices]
+
+print features_neworder
+print ("Feature ranking for combination 4: ")
+
+for f in range(X.shape[1]):
+    print ("%d. feature %d %s (%f)" % (f + 1, indices[f], comb4[indices[f]],  importances[indices[f]]))
+
+plt.figure()
+plt.title("Feature importance for combination 4")
+plt.bar(range(X.shape[1]), importances[indices], color="b", yerr=std[indices], align="center")
+plt.xticks(range(X.shape[1]), features_neworder)
+plt.xlim([-1, X.shape[1]])
+plt.savefig('../explore/comb4_features.png')
+
+print "Done with combination 4"
+
+print "Combination 5 - Desc features only -----------------------------------"
+
+X = train[comb5]
+y = train['Mood']
+
+
+forest = ExtraTreesClassifier(n_estimators=250)
+forest.fit(X,y)
+
+importances = forest.feature_importances_
+std = np.std([tree.feature_importances_ for tree in forest.estimators_], axis=0)
+indices = np.argsort(importances)[::-1]
+
+features_neworder = [comb5[i] for i in indices]
+
+print features_neworder
+print ("Feature ranking for combination 5: ")
+
+for f in range(X.shape[1]):
+    print ("%d. feature %d %s (%f)" % (f + 1, indices[f], comb5[indices[f]],  importances[indices[f]]))
+
+plt.figure()
+plt.title("Feature importance for combination 5")
+plt.bar(range(X.shape[1]), importances[indices], color="b", yerr=std[indices], align="center")
+plt.xticks(range(X.shape[1]), features_neworder)
+plt.xlim([-1, X.shape[1]])
+plt.savefig('../explore/comb5_features.png')
+
+
+print "Done with combination 5"
+
+print " -------------------------------------------------------------------"
+
+print "Elapsed time", time() - start
+
